6f9b48308001b8329ce768cb03ddd115e1aceb02
root <root@Moniems_Laptop.localdomain>
2022-07-29 19:31:40 -0400

    Commit Script

--- src/grid.py
+++ src/grid.py
@@ -15,15 +15,19 @@ class grid():
 
     for snake in board['snakes']:
       for part in snake['body']:
-        self.grid[part['y']][part['x']].isOtherSnakeBody()
+        cell = self.grid[part['y']][part['x']]
+        cell.isOtherSnakeBody = True
+        cell.isObstacle = True
       head = snake['head']
-      self.grid[head['y']['x']].isOtherSnakeHead()
+      self.grid[head['y']['x']].isOtherSnakeHead = True
+      self.grid[head['y']['x']].isObstacle = True
 
     for part in board['you']['body']:
-      self.grid[part['y']][part['x']].isSelf()
+      self.grid[part['y']][part['x']].isSelf = True
+      self.grid[part['y']][part['x']].isObstacle = True
 
     myHead = board['you']['head']
-    self.grid[myHead['y']][myHead['x']].isHead()
+    self.grid[myHead['y']][myHead['x']].isHead = True
 
   def getGrid(self):
     return self.grid
